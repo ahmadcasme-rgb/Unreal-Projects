9c7d03390d20eb4d41d85630dbeb0a25c3b9874d
Ahmad Kazmi <ahmadcasme@gmail.com>
2026-08-30 01:03:07 +0530

    IsKeyPlaced Function

--- DungeonQuest/Source/DungeonQuest/Lock.cpp
+++ DungeonQuest/Source/DungeonQuest/Lock.cpp
@@ -25,7 +25,8 @@ ALock::ALock()
 void ALock::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	SetIsKeyPlaced(true);
 }
 
 // Called every frame
@@ -33,5 +34,18 @@ void ALock::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ALock::SetIsKeyPlaced(bool NewIsKeyPlaced)
+{
+	IsKeyPlaced = NewIsKeyPlaced;
+
+	TriggerComp->Trigger(NewIsKeyPlaced);
+	KeyItemMesh->SetVisibility(NewIsKeyPlaced);
+}
+
+bool ALock::GetIsKeyPlaced()
+{
+	return IsKeyPlaced;
 }
 
